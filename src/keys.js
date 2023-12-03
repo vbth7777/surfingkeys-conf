1a8624e84f684fd5e784da6ebe1519b4ed92c6a0
vbth7777 <vbth7777@gmail.com>
2023-12-03 21:21:33 +0700

    03/12/2023

--- src/keys.js
+++ src/keys.js
@@ -46,13 +46,11 @@ const unmaps = {
     s: ["g", "d", "b", "e", "w", "s", "h", "y"],
   },
 }
-  //Configurate for specific sites
-  (() => {
-    const { hostname } = window.location
-    if (hostname.includes("pixiv")) {
-      unmaps.mappings.push("z")
-    }
-  })()
+//Configurate for specific sites
+const { hostname } = window.location
+if (hostname.includes("pixiv")) {
+  unmaps.mappings.push("z")
+}
 
 const maps = {}
 
