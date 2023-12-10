a0c0567def28372b6acf0b0dceff7ff68b23c77d
vbth7777 <vbth7777@gmail.com>
2023-12-11 01:21:10 +0700

    11/12/2023

--- src/actions.js
+++ src/actions.js
@@ -1511,10 +1511,10 @@ actions.nh.createViewer = async (idGallery) => {
       img.style.position = 'absolute'
       img.style.top = '0';
       img.style.left = '0';
-      // img.style.width = sizeImage//sizePercent + '%';
+      img.style.width = sizeImage//sizePercent + '%';
       // img.style.height = 'auto';
       img.onload = () => {
-        img.style.width = sizeImage//sizePercent + '%';
+        // img.style.width = sizeImage//sizePercent + '%';
         img.style.height = img.height;
       }
       img.style.objectFit = 'cover';
