4230371cfc73a6a6291297c256206b3cdd46eb71
vbth7777 <vbth7777@gmail.com>
2024-01-11 18:49:40 +0700

    11/01/2024

--- src/actions.js
+++ src/actions.js
@@ -1521,14 +1521,14 @@ actions.nh.createViewer = async (idGallery) => {
       img.style.objectFit = 'cover';
       img.loading = 'lazy';
       img.onerror = () => {
-        const changeServer = (serverNumber, format) => {
-          return img.src.replace(/\/\/i\d+/g, '//i' + serverNumber).replace(/\.(jpg|png)$/, format);
-        }
-        img.src = changeServer(server[++counter], format)
-        if (counter >= 2) {
-          format = 'png';
-          counter = -1;
-        }
+        // const changeServer = (serverNumber, format) => {
+        //   return img.src.replace(/\/\/i\d+/g, '//i' + serverNumber).replace(/\.(jpg|png)$/, format);
+        // }
+        // img.src = changeServer(server[++counter], format)
+        // if (counter >= 2) {
+        //   format = 'png';
+        //   counter = -1;
+        // }
 
         // if (img.src.includes('i5') && img.src.includes('jpg')) {
         //   img.src = img.src.replace('i5', 'i3');
@@ -1552,9 +1552,9 @@ actions.nh.createViewer = async (idGallery) => {
       imgTemp.src = img.src.replace('.jpg', 't.jpg').replace('.png', 't.png').replace(/\/\/i\d+/g, '//t3');
       imgTemp.onerror = () => {
         const changeServer = (serverNumber, format) => {
-          return img.src.replace(/\/\/i\d+/g, '//i' + serverNumber).replace(/\.(jpg|png)$/, format);
+          return img.src.replace(/\/\/i\d+/g, '//i' + serverNumber).replace(/\.(jpg|png)$/, '.' + format);
         }
-        console.log(imgTemp.src, changeServer(server[++counter], format))
+        console.log(imgTemp.src, counter)
         imgTemp.src = changeServer(server[++counter], format)
         if (counter >= 2) {
           format = 'png';
