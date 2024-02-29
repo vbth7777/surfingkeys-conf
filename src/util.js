0fab1fb289d4bb94e3ab63205c812642a44ab742
vbth7777 <vbth7777@gmail.com>
2024-03-01 00:15:25 +0700

    01/03/2024

--- src/util.js
+++ src/util.js
@@ -483,6 +483,11 @@ util.createComicViewer = async (images, imagesPerPage, previewImages, infomation
         // if (!imgTemp.complete) {
         //   imgTemp.src = img.src;
         // }
+        // setTimeout(() => {
+        //   const temp = img.src;
+        //   img.src = '';
+        //   img.src = temp;
+        // }, 500)
         const temp = img.src;
         img.src = '';
         img.src = temp;
